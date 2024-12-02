d6bf647923e7a593519158d3cd27a5593ea55149
Juan Pablo Garrido Linares <jpgl.garrido.linares@gmail.com>
2024-12-02 11:32:27 +0100

    package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, homeOutline, settingsOutline, cartOutline, basketOutline, personOutline, pizzaOutline, bookOutline } from 'ionicons/icons';
 import { User } from './models/user.model';
 import { UserService } from './service/user.service';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
@@ -26,9 +27,16 @@ export class AppComponent {
     addIcons({cartOutline,basketOutline,personOutline,pizzaOutline,bookOutline,homeOutline,heartOutline,settingsOutline,mailOutline,mailSharp,paperPlaneOutline,paperPlaneSharp,heartSharp,archiveOutline,archiveSharp,trashOutline,trashSharp,warningOutline,warningSharp,bookmarkOutline,bookmarkSharp});
     this.userService.getUser().then(async (user: any) => {
       this.user = user;
-      //console.log('User: ', this.user);
+      this.initializeApp();
     });
 
+  }
+
+  initializeApp() {
+    SplashScreen.show({
+      showDuration: 3000,
+      autoHide: true,
+    });
   }
 
 }
